6f764e7ab3d81e9c966c9f8624d9e6c251809ba4
USAGI <hogouxian@gmail.com>
2017-08-15 04:38:40 +0800

    fixed bugs on registerCallback

--- Src/callback.c
+++ Src/callback.c
@@ -1,15 +1,16 @@
 #include "callback.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void registerCallback(void (callBack)(Event*), LinkedList *list){
   //create TxRxCallbackList item
   //create item to associate TxRxCallbackList
   //add inside linkedlist
-  static TxRxCallbackList callBackList;
-  callBackList.txRxCallbackFuncP = callBack;
-  static Item item;
-  item.data = (void*)&callBackList;
-  pushList(list, &item);
+  TxRxCallbackList *callBackList = (TxRxCallbackList*)malloc(sizeof(TxRxCallbackList));
+  callBackList->txRxCallbackFuncP = callBack;
+  Item *item = (Item*)malloc(sizeof(Item));
+  item->data = (void*)callBackList;
+  pushList(list, item);
 }
 
 void *unregisterCallback(){
--- test/test_callback.c
+++ test/test_callback.c
@@ -11,7 +11,6 @@ void tearDown(void)
 }
 
 void someFunction(Event* evt){
-  printf("success!!\n");
 }
 void somethingBetween(){
 
@@ -33,20 +32,8 @@ void test_registerCallback_given_emptyList(void){
 void test_registerCallback_given_list_with_1_data(void){
   LinkedList list;
   ListInit(&list);
-  /*registerCallback(anotherFunction, &list);
-  registerCallback(someFunction, &list);*/
-
-  TxRxCallbackList callBackList;
-  callBackList.txRxCallbackFuncP = someFunction;
-  Item item;
-  item.data = (void*)&callBackList;
-  pushList(&list, &item);
-
-  TxRxCallbackList callBackList1;
-  callBackList1.txRxCallbackFuncP = anotherFunction;
-  Item item1;
-  item1.data = (void*)&callBackList1;
-  pushList(&list, &item1);
+  registerCallback(someFunction, &list);
+  registerCallback(anotherFunction, &list);
 
   Item *itemHead = list.head;
   TxRxCallbackList *callBackList5 = (TxRxCallbackList*)(itemHead->data);
@@ -55,8 +42,4 @@ void test_registerCallback_given_list_with_1_data(void){
   TxRxCallbackList *callBackListNext = (TxRxCallbackList*)(itemHead->next->data);
   TEST_ASSERT_EQUAL(2,list.len);
   TEST_ASSERT_EQUAL_PTR(someFunction, callBackListNext->txRxCallbackFuncP);
-  Event evt;
-  evt.evtType = UART_RX_SUCCESS;
-  callBackListNext->txRxCallbackFuncP(&evt);
-
 }
